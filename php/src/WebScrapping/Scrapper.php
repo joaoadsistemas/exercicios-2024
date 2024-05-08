71d27e08d811a7ba52b03216f61fb2c4b929315a
Joao Henrique <joaoadsistemas@gmail.com>
2024-05-08 16:54:20 -0300

    intial

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -8,23 +8,61 @@ use Chuva\Php\WebScrapping\Entity\Person;
 /**
  * Does the scrapping of a webpage.
  */
-class Scrapper {
+class Scrapper
+{
 
   /**
    * Loads paper information from the HTML and returns the array with the data.
    */
-  public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
+  public function scrap(\DOMDocument $dom): array
+  {
+    $identifierElement = $dom->getElementById('hits-container');
+    $result = [];
 
+    if ($identifierElement) {
+      $mainElement = $identifierElement->parentNode;
+
+      foreach ($mainElement->childNodes as $child) {
+        $id = null;
+        $title = null;
+        $type = null;
+        $authors = [];
+
+        if ($child instanceof \DOMElement) {
+          if ($child->nodeName === 'a') {
+            $link = $child->hasAttribute('href') ? $child->getAttribute('href') : null;
+            $linkParts = explode('/', $link);
+            $id = (int) end($linkParts);
+
+            foreach ($child->childNodes as $elementHTML) {
+              if ($elementHTML instanceof \DOMElement) {
+                $htmlClass = $elementHTML->getAttribute('class');
+
+                if (str_contains($htmlClass, 'title')) {
+                  $title = $elementHTML->nodeValue;
+                } elseif (str_contains($htmlClass, 'authors')) {
+
+                  foreach ($elementHTML->childNodes as $author) {
+                    if ($author instanceof \DOMElement && $author->getAttribute('title')) {
+                      $name = trim($author->nodeValue);
+                      $name = str_replace(';', '', $name);
+                      $institution = $author->getAttribute('title');
+
+                      $authors[] = new Person($name, $institution);
+                    }
+                  }
+                } else {
+                  $type = $elementHTML->firstChild->nodeValue;
+                }
+              }
+            }
+
+            $result[] = new Paper($id, $title, $type, $authors);
+          }
+        }
+      }
+    }
+
+    return $result;
+  }
 }
---
+++ php/src/WebScrapping/Writer.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Chuva\Php\WebScrapping;
+
+use Box\Spout\Common\Entity\Row;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+
+class Writer
+{
+    public function write($data): void
+    {
+        $filePath = __DIR__ . '/data.xlsx';
+        $maxAuthors = 0;
+
+        $writer = WriterEntityFactory::createXLSXWriter();
+        $writer->openToFile($filePath);
+
+        foreach ($data as $row) {
+            $maxAuthors = max($maxAuthors, count($row->authors));
+        }
+
+        $headerCells = [
+            WriterEntityFactory::createCell('ID'),
+            WriterEntityFactory::createCell('Title'),
+            WriterEntityFactory::createCell('Type'),
+        ];
+
+        for ($i = 1; $i <= $maxAuthors; $i++) {
+            $headerCells[] = WriterEntityFactory::createCell("Author $i");
+            $headerCells[] = WriterEntityFactory::createCell("Institution $i");
+        }
+
+        $headerRow = WriterEntityFactory::createRow($headerCells);
+        $writer->addRow($headerRow);
+
+        foreach ($data as $row) {
+            $cells = [
+                WriterEntityFactory::createCell($row->id),
+                WriterEntityFactory::createCell($row->title),
+                WriterEntityFactory::createCell($row->type),
+            ];
+
+            foreach ($row->authors as $author) {
+                $cells[] = WriterEntityFactory::createCell($author->name);
+                $cells[] = WriterEntityFactory::createCell($author->institution);
+            }
+
+            $singleRow = WriterEntityFactory::createRow($cells);
+            $writer->addRow($singleRow);
+        }
+
+        $writer->close();
+    }
+}
